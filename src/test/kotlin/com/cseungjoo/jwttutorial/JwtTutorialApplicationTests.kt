af8fde9b2ee93814ad071996c23a7ba3ce02fb01
CSeungJoo <seungjoo13579@gmail.com>
2024-06-05 17:53:30 +0900

    init

---
+++ src/test/kotlin/com/cseungjoo/jwttutorial/JwtTutorialApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cseungjoo.jwttutorial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JwtTutorialApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
